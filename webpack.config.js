667b6e532d1f321a45e95eb44d670175599bbabc
ermacaz <ermacaz@gmail.com>
2022-04-10 13:44:59 -0700

    attempt to fix js asset compiling

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const mode = process.env.NODE_ENV === 'development' ? 'development' : 'productio
 
 
 module.exports = {
-  mode: mode,
+  mode: 'production',
   devtool: 'source-map',
   entry: {
     application: ["./app/javascript/application.js"]
